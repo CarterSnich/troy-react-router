fe1ecc4ca54af56df5f90db86c6a9426316802b0
CarterSnich <jmestreraclosa@gmail.com>
2022-12-05 23:14:03 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact-us" element={<ContactUs />} />
+        </Route>
+      </Routes>
+
     </div>
   );
 }
 
+
+
+
 export default App;
+
+
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact-us">Contact us</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home page</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About page</h2>
+    </div>
+  );
+}
+
+function ContactUs() {
+  return (
+    <div>
+      <h2>Contact us page</h2>
+    </div>
+  );
+}
